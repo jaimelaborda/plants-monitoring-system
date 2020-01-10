ddbd2285232c4acf90de3fb6fb8f10621f65e94f
Jaime Laborda <jaimelaborda@gmail.com>
2020-01-10 18:25:29 +0100

    Update sensor code to work with new MQTT broker

---
+++ sensor-planta/config_example.h
@@ -0,0 +1,11 @@
+const char* ssid = "........";
+const char* password = "........";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+const int* mqtt_port = 1883;
+const char* mqtt_user = "mqtt_user";
+const char* mqtt_password = "mqtt_password";
+
+const char* mqtt_topic_temperature = "planta/temperature";
+const char* mqtt_topic_humidity = "planta/humidity";
+const char* mqtt_topic_lux = "planta/lux";
+const char* mqtt_topic_soil_humidity = "planta/soil_humidity";
--- sensor-planta/sensor-planta.ino
+++ sensor-planta/sensor-planta.ino
@@ -1,314 +1,165 @@
 /***************************************************
-  Planta Twittera 2r0
-  by Jaime Laborda
-  MakersUPV
+  Plant monitoring system v1
+  by Jaime Laborda & Alberto Diaz
+  Sistemas Embebidos - MUISE
  ****************************************************/
 
-// Adafruit IO
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "aio_username"
-#define AIO_KEY         "aio_key"  // Obtained from account info on io.adafruit.com
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <BH1750.h>
+#include "DHTesp.h"
+#include "config.h" //Credentials are store here
 
-// DHT 11 sensor
 #define DHTPIN 14 //D5
-#define DHTTYPE DHT11
-
-// maximum number of seconds between resets that
-// counts as a double reset
-#define DRD_TIMEOUT 2.0
-#define DRD_ADDRESS 0x00
-
-//Sleep interval
-#define SLEEP_INTERVAL "1" //1 minute
+#define DHTTYPE DHTesp::DHT11
 
 //Calibration soil sensor
 #define SOIL_HUM_100 100
 #define SOIL_HUM_0 430
 
-// Libraries
-#include <FS.h>
-#include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
-#include <ArduinoJson.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
-#include "DHT.h"
-#include <Wire.h>
-#include <BH1750.h>
+// Update these with values suitable for your network.
 
-//for LED status
-#include <Ticker.h>
-Ticker ticker;
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
-//Double Reset Detector
-#include <DoubleResetDetect.h>
-DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
-
-
-
-bool shouldSaveConfig = false;
-
-
-void tick()
-{
-  //toggle state
-  bool state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
-  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
-}
-
-//gets called when WiFiManager enters configuration mode
-void configModeCallback (WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
-  Serial.println(myWiFiManager->getConfigPortalSSID());
-  //entered config mode, make led toggle faster
-  ticker.attach(0.1, tick);
-}
-
-// DHT sensor
-DHT dht(DHTPIN, DHTTYPE, 15);
-
-//Lux sensor
+DHTesp dht;
 BH1750 lightMeter;
 
+float humidity;
+float temperature;
+float lux;
+int soil_humidity;
 
+void readSensors(){
 
-/*************************** Sketch Code ************************************/
+  Serial.println("Reading sensors:");
+  Serial.println("Temp  RH   Lux   Soil");
+  temperature = dht.getTemperature(); //in Celsius
+  humidity = dht.getHumidity(); //getHumidity
+  lux = lightMeter.readLightLevel();
+  soil_humidity = map(analogRead(0), SOIL_HUM_0, SOIL_HUM_100 , 0, 100);
+
+  Serial.print(temperature);
+  Serial.print(" | ");
+  Serial.print(humidity);
+  Serial.print(" | ");
+  Serial.print(lux);
+  Serial.print(" | ");
+  Serial.println(soil_humidity);
+}
 
 void setup() {
-
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
-  //set led pin as output
-
-  pinMode(BUILTIN_LED, OUTPUT);
-
-  // Init sensor
-  dht.begin();
-
-  //Lux sensor
   Wire.begin();
-  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+  
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 
-  //set led pin as output
-  pinMode(BUILTIN_LED, OUTPUT);
-  pinMode(0, INPUT);
-  // start ticker with 0.5 because we start in AP mode and try to connect
-  ticker.attach(0.5, tick);
 
-  //define your default values here, if there are different values in config.json, they are overwritten.
-  char* aio_username = AIO_USERNAME;
-  char* aio_key = AIO_KEY;
-  char* sleep_interval = SLEEP_INTERVAL; //minutes
+  // T & RH Sensor
+  dht.setup(DHTPIN, DHTTYPE); // Connect DHT sensor to GPIO 17
+  // Light sensor
+  lightMeter.begin();
+}
 
-  //read configuration from FS json
-  Serial.println("mounting FS...");
+void setup_wifi() {
 
-  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial.println("opened config file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-        configFile.readBytes(buf.get(), size);
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject(buf.get());
-        json.printTo(Serial);
-        if (json.success()) {
-          Serial.println("\nparsed json");
-          strcpy(aio_username, json["param_aio_username"]);
-          strcpy(aio_key, json["param_aio_key"]);
-          strcpy(sleep_interval, json["sleep_interval"]); //minutes
+  WiFi.begin(ssid, password);
 
-          Serial.print("Loaded aio_username: ");
-          Serial.println(aio_username);
-          Serial.print("Loaded aio_key: ");
-          Serial.println(aio_key);
-          Serial.print("Loaded sleep_interval: ");
-          Serial.println(sleep_interval);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-        } else {
-          Serial.println("failed to load json config");
-        }
-      }
-    }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
   } else {
-    Serial.println("failed to mount FS");
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
 
-  WiFiManager wifiManager;
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
+}
 
-  //WifiManager & custom parameters
-  WiFiManagerParameter custom_aio_username("aio_username", "AIO USERNAME", aio_username, 40);
-  WiFiManagerParameter custom_aio_key("aio_key", "AIO KEY", aio_key, 40);
-  WiFiManagerParameter custom_sleep_interval("sleep_interval", "INTERVAL", sleep_interval, 40);
-
-  wifiManager.addParameter(&custom_aio_username);
-  wifiManager.addParameter(&custom_aio_key);
-  wifiManager.addParameter(&custom_sleep_interval);
-
-
-  if (drd.detect()) {
-    Serial.println("** Double reset boot **");
-    wifiManager.resetSettings();
-  }
-
-  //wifiManager.resetSettings();
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  wifiManager.setAPCallback(configModeCallback);
-
-  if (!wifiManager.autoConnect("Planta Twittera")) {
-    Serial.println("failed to connect and hit timeout");
-    //reset and try again, or maybe put it to deep sleep
-    ESP.reset();
-    delay(1000);
-  }
-  //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
-  ticker.detach();
-  //keep LED on
-  digitalWrite(BUILTIN_LED, LOW);
-
-  strcpy(aio_username, custom_aio_username.getValue());
-  strcpy(aio_key, custom_aio_key.getValue());
-  strcpy(sleep_interval, custom_sleep_interval.getValue());
-
-  if (shouldSaveConfig) {
-    shouldSaveConfig = false;
-
-    //save the custom parameters to FS
-    Serial.println("saving config");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-
-    json["param_aio_username"] = aio_username;
-    json["param_aio_key"] = aio_key;
-    json["sleep_interval"] = sleep_interval;
-
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("esp8266_planta", mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
-
-    json.printTo(Serial);
-    json.printTo(configFile);
-    configFile.close();
-    //end save
-    Serial.println("FS Config Updated");
   }
-
-  // Create an ESP8266 WiFiClient class to connect to the MQTT server.
-  WiFiClient client;
-
-  String str_aio_username = String(AIO_USERNAME);
-  String str_aio_key = String(AIO_KEY);
-
-  // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-  Serial.println("ParametersClient: " + String(aio_username) + " " + String(aio_key));
-  Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, str_aio_username.c_str(), str_aio_key.c_str());
-
-  // Parameters feed configuration
-  String str_temperature = str_aio_username + "/feeds/temperature";
-  String str_humidity = str_aio_username + "/feeds/humidity";
-  String str_soil_humidity = str_aio_username + "/feeds/soil_humidity";
-  String str_luminosity = str_aio_username + "/feeds/luminosity";
-
-  Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, str_temperature.c_str());
-  Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, str_humidity.c_str());
-  Adafruit_MQTT_Publish soil_humidity = Adafruit_MQTT_Publish(&mqtt, str_soil_humidity.c_str());
-  Adafruit_MQTT_Publish luminosity = Adafruit_MQTT_Publish(&mqtt, str_luminosity.c_str());
-
-  // connect to adafruit io
-
-  Serial.print(F("Connecting to Adafruit IO... "));
-
-  int8_t ret;
-
-  while ((ret = mqtt.connect()) != 0) {
-
-    switch (ret) {
-      case 1: Serial.println(F("Wrong protocol")); break;
-      case 2: Serial.println(F("ID rejected")); break;
-      case 3: Serial.println(F("Server unavail")); break;
-      case 4: Serial.println(F("Bad user/pass")); break;
-      case 5: Serial.println(F("Not authed")); break;
-      case 6: Serial.println(F("Failed to subscribe")); break;
-      default: Serial.println(F("Connection failed")); break;
-    }
-
-    if (ret >= 0)
-      mqtt.disconnect();
-
-    Serial.println(F("Retrying connection..."));
-    delay(5000);
-
-  }
-
-  Serial.println(F("Adafruit IO Connected!"));
-
-  //Aqui hago cosas
-
-  // Grab the current state of the sensor
-  int humidity_data = (int)dht.readHumidity();
-  int temperature_data = (int)dht.readTemperature();
-
-  // By default, the temperature report is in Celsius, for Fahrenheit uncomment
-  //    following line.
-  // temperature_data = temperature_data*(9.0/5.0) + 32.0;
-
-  // Read Soil Humidity
-  int soil_humidity_data = (int)readSoilHumidity();
-
-  // Lux sensor
-  uint16_t luminosity_data = lightMeter.readLightLevel();
-
-  // Publish data
-  if (! temperature.publish(temperature_data))
-    Serial.println(F("Failed to publish temperature"));
-  else
-    Serial.println(F("Temperature published!"));
-
-  if (! humidity.publish(humidity_data))
-    Serial.println(F("Failed to publish humidity"));
-  else
-    Serial.println(F("Humidity published!"));
-
-  if (! soil_humidity.publish(soil_humidity_data))
-    Serial.println(F("Failed to publish soil humidity"));
-  else
-    Serial.println(F("Soil Humidity published!"));
-
-  if (! luminosity.publish(luminosity_data))
-    Serial.println(F("Failed to publish luminosity"));
-  else
-    Serial.println(F("Luminosity published!"));
-
-  delay(2000);
-
-  //Deep sleep to save power
-  ESP.deepSleep(atoi(sleep_interval) * 60e6); //in seconds
 }
 
 void loop() {
-  //Do nothing in the loop
-}
 
-float readSoilHumidity () {
-  return map(analogRead(0), SOIL_HUM_0, SOIL_HUM_100 , 0, 100);
-}
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-void saveConfigCallback () {
-  shouldSaveConfig = true;
-  //ticker.attach(0.5, tick);
-}
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
 
+    //Leo los sensores
+    readSensors();
+
+    //Publico
+    char temperature_str[10]; 
+    char humidity_str[10]; 
+    char lux_str[10];
+    char soil_humidity_str[10];
+    
+    sprintf(temperature_str, "%.2f", temperature); //Convierto a string temperatura
+    sprintf(humidity_str, "%.2f", humidity); //Convierto a string humedad
+    sprintf(lux_str, "%.2f", lux); //Convierto a string lux
+    sprintf(soil_humidity_str, "%d", soil_humidity);
+    
+    client.publish(mqtt_topic_temperature, temperature_str);
+    client.publish(mqtt_topic_humidity, humidity_str);
+    client.publish(mqtt_topic_lux, lux_str);
+    client.publish(mqtt_topic_soil_humidity, soil_humidity_str);
+
+    Serial.println("Message published!\n");
+  }
+}
